857cff7712909ba8ef53433e2cc11ad7021c28af
Sam <samdrugan@gmail.com>
2018-01-03 14:55:31 +0000

    added update function to player for movement

--- player.py
+++ player.py
@@ -6,6 +6,8 @@ class Player:
     def __init__(self,screen, playerNum, color, startX, startY, controlType):
         self.x = startX;
         self.y = startY;
+        self.xSpeed = 0;
+        self.ySpeed = 0;
         self.screen = screen;
         self.attack = 1;
         self.defence = 1;
@@ -20,17 +22,14 @@ class Player:
         self.direction = 0;
         self.dashCooldown = 1;
         self.playerNum = playerNum;
-        self.right = False;
-        self.left = False;
-        self.up = False;
-        self.down = False;
+        self.right = 0;
+        self.left = 0;
+        self.up = 0;
+        self.down = 0;
         self.vertices = [];
         self.controlType = controlType #keyboard/joystick
         
-        self.calculateVectors()
-    
-    
-    def calculateVectors(self):
+        
         for i in range(12):
             radians = math.radians(i * 30);
             self.vertices.append([]);
@@ -44,7 +43,21 @@ class Player:
             currentVertices.append([])
             currentVertices[i].append(self.vertices[i][0] + self.x);
             currentVertices[i].append(self.vertices[i][1] + self.y);
+            
+        pygame.draw.polygon(self.screen, (0,0,255), currentVertices);
         pygame.draw.lines(self.screen, self.color, True, currentVertices, 2);
+        
+    
+    def update(self):
+        xSpeed = self.right + self.left;
+        ySpeed = self.up + self.down;
+        maxSpeed = self.speed
+        if math.fabs(xSpeed + ySpeed) == 2:
+            maxSpeed = math.floor(maxSpeed*0.7 + 0.5);
+            
+        self.x += xSpeed*maxSpeed;
+        self.y += ySpeed*maxSpeed;        
+        
         
     
         
