23137480d856d91b37ea7331784054d2aa79215c
Joey <joeydelarago@rocketmail.com>
2018-01-03 12:44:55 +0000

    Added functions for keyup and keydown events and placeholders for other functions. removed the files combat.py and controls.py as they are redundant.

--- game_functions.py
+++ game_functions.py
@@ -1,1 +1,45 @@
 import pygame
+from time import sleep
+import sys
+from player import Player
+
+
+def keydown_events(event, screen, player):
+    if player.controlType == "keyboard":
+        if event.key == pygame.K_UP:
+            player.up = True
+        if event.key == pygame.K_RIGHT:
+            player.right = True
+        if event.key == pygame.K_DOWN:
+            player.down = True
+        if event.key == pygame.K_LEFT:
+            player.left = True
+    if player.controlType == "keyboard":
+        pass
+
+
+def keyup_events(event, screen, player):
+    if player.controlType == "keyboard":
+        if event.key == pygame.K_UP:
+            player.up = False
+        if event.key == pygame.K_RIGHT:
+            player.right = False
+        if event.key == pygame.K_DOWN:
+            player.down = False
+        if event.key == pygame.K_LEFT:
+            player.left = False
+    if player.controlType == "keyboard":
+        pass
+
+
+def update_screen(screen):
+    screen.fill("red")
+
+
+def update_player():
+    player.draw()
+
+
+def check_player_collisions():
+    pass
+
