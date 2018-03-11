42ddb3533b28e7453677720e75e68a11ed5e6225
Joey <joeydelarago@rocketmail.com>
2018-03-11 14:36:31 +0000

    Added changable arena backgrounds and changable opacity for sidesbars.

--- arena.py
+++ arena.py
@@ -1,47 +1,80 @@
 import pygame
 
+
 class Arena(object):
-    #Draws the arena.
+    # Draws the arena.
     def __init__(self, settings, screen):
         self.settings = settings
         self.screen = screen
-        self.background = pygame.image.load("assets/arena.png").convert()
+        self.background = pygame.image.load(self.settings.background()).convert()
+        self.color = (0, 0, 0)
+        self.sidebar_color = (0, 0, 0)
+        self.text_color = (230, 230, 230)
+        self.font = "assets/fonts/Montserrat-Medium.ttf"
+        # color for arena overlay
+        self.arena = pygame.Surface((self.settings.arena_dimension,
+                                     self.settings.arena_dimension))
+        self.arena.set_alpha(0)
+        #  color overlay for right bar
+        self.arenar = pygame.Surface((self.settings.arena_x,
+                                      self.settings.arena_dimension))
+        self.arenar.set_alpha(120)
+        # color overlay for left bar
+        self.arenal = pygame.Surface((self.settings.arena_x,
+                                      self.settings.arena_dimension))
+        self.arenal.set_alpha(120)
 
     def draw_arena(self):
-        #Draws the arena for the players to fight in.
-        self.screen.fill((0, 0, 0))
-        self.screen.fill(self.settings.arenaColor, (self.settings.arena_x, 0, self.settings.arena_dimension,self.settings.arena_dimension))
-        self.screen.blit(self.background, (self.settings.arena_x, 0, self.settings.arena_dimension,self.settings.arena_dimension))
+        # Draws the arena for the players to fight in
+        arena_x = self.settings.arena_x
+        arena_size = self.settings.arena_dimension
+        color = self.settings.arenaColor
+        self.screen.blit(self.background, (0, 0))
+        self.arena.fill(self.color)
+        self.screen.blit(self.arena, (arena_x, 0))
 
     def draw_game_sidebars(self):
-        #Draws the sidebars in the gameloop that indicate the players statuses.
+        t = self.text_color
+        f = self.font
+        s = self.screen
+
+        # Fills the left bar with a transparent color
+        self.arenal.fill(self.sidebar_color)
+        self.screen.blit(self.arenal, (0, 0))
+        # Fills the right bar with a transparent color
+        self.arenar.fill(self.sidebar_color)
+        self.screen.blit(self.arenar, (self.settings.arena_x +
+                                       self.settings.arena_dimension, 0))
+
+        # Draws the sidebars in the gameloop that indicate the players statuses
         screenx = self.settings.resolution()[0]
         screeny = self.settings.resolution()[1]
         borderx = (screenx - screeny) // 2
-        player_info = pygame.font.Font("assets/fonts/Montserrat-Medium.ttf", screenx // 20).render("Player 1", 0, (255, 0, 0))
-        self.screen.blit(player_info, (0, 0))
-        player_info = pygame.font.Font("assets/fonts/Montserrat-Medium.ttf", screenx // 25).render("Wins:", 0, (255, 0, 0))
-        self.screen.blit(player_info, (0, screeny // 10))
-        player_info = pygame.font.Font("assets/fonts/Montserrat-Medium.ttf", screenx // 25).render("Losses:", 0, (255, 0, 0))
-        self.screen.blit(player_info, (0, screeny // 5))
 
-        player_info = pygame.font.Font("assets/fonts/Montserrat-Medium.ttf", screenx // 20).render("Player 2", 0, (0, 255, 0))
-        self.screen.blit(player_info, (0, screeny // 2))
-        player_info = pygame.font.Font("assets/fonts/Montserrat-Medium.ttf", screenx // 25).render("Wins:", 0, (0, 255, 0))
-        self.screen.blit(player_info, (0, (screeny // 2) + screeny // 10))
-        player_info = pygame.font.Font("assets/fonts/Montserrat-Medium.ttf", screenx // 25).render("Losses:", 0, (0, 255, 0))
-        self.screen.blit(player_info, (0, (screeny // 2) + screeny // 5))
+        stats = pygame.font.Font(f, screenx // 20).render("Player 1", 0, t)
+        s.blit(stats, (0, 0))
+        stats = pygame.font.Font(f, screenx // 25).render("Wins:", 0, t)
+        s.blit(stats, (0, screeny // 10))
+        stats = pygame.font.Font(f, screenx // 25).render("Losses:", 0, t)
+        s.blit(stats, (0, screeny // 5))
 
-        player_info = pygame.font.Font("assets/fonts/Montserrat-Medium.ttf", screenx // 20).render("Player 3", 0, (0, 0, 255))
-        self.screen.blit(player_info, (screenx - (borderx), 0))
-        player_info = pygame.font.Font("assets/fonts/Montserrat-Medium.ttf", screenx // 25).render("Wins:", 0, (0, 0, 255))
-        self.screen.blit(player_info, (screenx - (borderx), screeny // 10))
-        player_info = pygame.font.Font("assets/fonts/Montserrat-Medium.ttf", screenx // 25).render("Losses:", 0, (0, 0, 255))
-        self.screen.blit(player_info, (screenx - (borderx), screeny // 5))
+        stats = pygame.font.Font(f, screenx // 20).render("Player 2", 0, t)
+        s.blit(stats, (0, screeny // 2))
+        stats = pygame.font.Font(f, screenx // 25).render("Wins:", 0, t)
+        s.blit(stats, (0, (screeny // 2) + screeny // 10))
+        stats = pygame.font.Font(f, screenx // 25).render("Losses:", 0, t)
+        s.blit(stats, (0, (screeny // 2) + screeny // 5))
 
-        player_info = pygame.font.Font("assets/fonts/Montserrat-Medium.ttf", screenx // 20).render("Player 4", 0, (255, 255, 0))
-        self.screen.blit(player_info, (screenx - (borderx), screeny // 2))
-        player_info = pygame.font.Font("assets/fonts/Montserrat-Medium.ttf", screenx // 25).render("Wins:", 0, (255, 255, 0))
-        self.screen.blit(player_info, (screenx - (borderx), screeny // 2 + screeny // 10))
-        player_info = pygame.font.Font("assets/fonts/Montserrat-Medium.ttf", screenx // 25).render("Losses:", 0, (255, 255, 0))
-        self.screen.blit(player_info, (screenx - (borderx), screeny // 2 + screeny // 5))
+        stats = pygame.font.Font(f, screenx // 20).render("Player 3", 0, t)
+        s.blit(stats, (screenx - (borderx), 0))
+        stats = pygame.font.Font(f, screenx // 25).render("Wins:", 0, t)
+        s.blit(stats, (screenx - (borderx), screeny // 10))
+        stats = pygame.font.Font(f, screenx // 25).render("Losses:", 0, t)
+        s.blit(stats, (screenx - (borderx), screeny // 5))
+
+        stats = pygame.font.Font(f, screenx // 20).render("Player 4", 0, t)
+        s.blit(stats, (screenx - (borderx), screeny // 2))
+        stats = pygame.font.Font(f, screenx // 25).render("Wins:", 0, t)
+        s.blit(stats, (screenx - (borderx), screeny // 2 + screeny // 10))
+        stats = pygame.font.Font(f, screenx // 25).render("Losses:", 0, t)
+        s.blit(stats, (screenx - (borderx), screeny // 2 + screeny // 5))
--- settings.py
+++ settings.py
@@ -2,18 +2,23 @@ import pygame
 
 
 class Settings(object):
-    #Holds the values for all settings in the game. Also hold the list of
-    #current players, possible resolutions and living players.
+    # Holds the values for all settings in the game. Also hold the list of
+    # current players, possible resolutions and living players.
     def __init__(self, height, width):
         self.screensize = (width, height)
         self.default_res = None
         self.fullscreen = True
         self.bg_color = (000, 100, 100)
+        self.bg_pointer = 0
+        self.background = lambda: self.backgrounds[self.bg_pointer]
+        self.backgrounds = ['assets/arena1.png', 'assets/arena2.png',
+                            'assets/arena3.png', 'assets/arena4.png',
+                            'assets/arena5.png']
         self.font = "assets/fonts/ShareTechMono-Regular.ttf"
         self.players = []
         self.state = "main"
         self.volume = 10
-        self.resolution = lambda : self.resolutions[self.respointer]
+        self.resolution = lambda: self.resolutions[self.respointer]
         self.resolutions = [(800, 600), (1024, 768), (1280, 768),
                             (1280, 1024), (1366, 768), (1600, 900),
                             (1920, 980), (1920, 1080)]
@@ -26,3 +31,6 @@ class Settings(object):
     def add_player(self, player):
         self.players.append(player)
         self.living_players += 1
+
+    def change_background(self):
+        self.bg_pointer = (self.bg_pointer + 1) % (len(self.backgrounds) - 1)
